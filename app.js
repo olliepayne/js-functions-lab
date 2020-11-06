684323c2886cb8322cedef6854dbee07034efa68
Oliver Payne <olliepayne@me.com>
2020-11-06 07:53:40 -0700

    fixed

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function isCharAVowel(aChar) {
         }
     });
 }
-isCharAVowel('p');
+isCharAVowel('o');
 
 
 // 4. Define a function, as a function expression, `sumArray` that takes an array of numbers and returns the sum of those numbers. For example, `sumArray([2, 4, 5]);` would return `11`.
